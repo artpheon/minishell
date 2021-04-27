8e48218d8d7b2b5ddfc727d01637258dd820d095
Howe Robbin <hrobbin@ge-l2.kzn.21-school.ru>
2021-04-27 15:09:42 +0300

    fixed shlvl

--- executor/env_utils.c
+++ executor/env_utils.c
@@ -7,7 +7,7 @@ void 	env_norm_extra(t_dict *tmp, char **elem)
 	size = ft_strlen(tmp->key) + ft_strlen(tmp->val) + 2;
 	*elem = malloc(size);
 	if (*elem == NULL)
-		exit(cmd_err("malloc", NULL, "malloc error"));
+		exit(cmd_err(SHELL, "malloc", strerror(errno)));
 	ft_strlcpy(*elem, tmp->key, size);
 	ft_strlcat(*elem, "=", size);
 	ft_strlcat(*elem, tmp->val, size);
@@ -48,14 +48,13 @@ t_dict	*envp_to_dict(char **envp)
 	i = 0;
 	while (envp[i])
 	{
-		strs = ft_split(envp[i], '=');
+		strs = ft_split_2(envp[i], '=');
 		if (i == 0)
-			new = dict_newentry(strs[0], strs[1]);
+			new = dict_newentry(ft_strdup(strs[0]), ft_strdup(strs[1]));
 		else
-			dict_addentry(&new, dict_newentry(strs[0], strs[1]));
-		free(strs[2]);
-		free(strs);
-		i++;
+			dict_addentry(&new, dict_newentry(ft_strdup(strs[0]), ft_strdup(strs[1])));
+		free_2(strs);
+		++i;
 	}
 	return (new);
 }
@@ -63,7 +62,7 @@ t_dict	*envp_to_dict(char **envp)
 void	shell_init_shlvl(t_dict **envp)
 {
 	int		i;
-	char	*itoa_tmp;
+	char	*tmp;
 
 	if (dict_isin(envp, "SHLVL"))
 	{
@@ -71,14 +70,14 @@ void	shell_init_shlvl(t_dict **envp)
 		if (i >= 999)
 		{
 			cmd_err("warning", NULL, "shell level reached 1000, reset to 1");
-			dict_chval(envp, "SHLVL", ft_strdup("1"));
+			tmp = ft_strdup("1");
 		}
+		else if (i < 0)
+			tmp = ft_strdup("1");
 		else
-		{
-			itoa_tmp = ft_itoa(i + 1);
-			dict_chval(envp, "SHLVL", ft_strdup(itoa_tmp));
-			free(itoa_tmp);
-		}
+			tmp = ft_itoa(i + 1);
+		dict_chval(envp, "SHLVL", ft_strdup(tmp));
+		free(tmp);
 	}
 	else
 		dict_addentry(envp, dict_newentry(ft_strdup("SHLVL"), ft_strdup("1")));
